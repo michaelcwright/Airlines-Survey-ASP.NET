07369baea6360378570c231f4f3ca50d2a1e6d60
Michael Wright <mwrightmichael@gmail.com>
2015-08-17 20:35:52 -0400

    Airlines Survey
    Airlines Survey

---
+++ Default.aspx
@@ -0,0 +1,102 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Airlines Survey</title>
+    <link href="stylesheet.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+     <div id="main">
+        <div id="mainC">
+            <div id="topHeader"></div>
+                <div id="bodyC">
+    <form id="form1" runat="server">
+     <br />
+    <br />
+    <h1>ZAirlines Survey</h1>
+    <br />
+    <br />
+    <p><b>Enter your Flight Number:</b></p>
+    <asp:TextBox ID="flightNumber" runat="server" align="center"></asp:TextBox>
+    <asp:RequiredFieldValidator ID="flightNumberValidate" runat="server"
+                                                    ControlToValidate="flightNumber" Display="Dynamic">**Required field**</asp:RequiredFieldValidator><br />
+    <asp:RegularExpressionValidator ID="maxFlight" 
+            runat="server" Display="dynamic" 
+            ControlToValidate="flightNumber" 
+            ValidationExpression="^([\S\s]{0,10})$" 
+            ErrorMessage="** Flight number should be no more than 10 digits **">
+    </asp:RegularExpressionValidator>
+    <p><b>Enter your Flight Date:</b></p>
+    <asp:TextBox ID="Date" runat="server" align="center"></asp:TextBox>
+    <asp:CompareValidator ID="compareDate" runat="server"
+            ControlToValidate="Date" Operator="DataTypeCheck"
+            Display="Dynamic" Type="Date">**You did not enter a
+            valid date **</asp:CompareValidator>
+
+    <p><b>Friendliness of customer staﬀ:</b></p>
+    <asp:RadioButtonList ID="friendStaff" EnableViewState="true" runat="server" align="center">
+    
+    <asp:ListItem Value="Excellent" runat="server" Text="Excellent"></asp:ListItem>
+    <asp:ListItem Value="Good" runat="server" Text="Good"></asp:ListItem>
+    <asp:ListItem Value="Fair" runat="server" Text="Fair"></asp:ListItem>
+    <asp:ListItem Value="Poor" runat="server" Text="Poor"></asp:ListItem>
+    <asp:ListItem Value="No Opinion" runat="server" Text="No Opinion"></asp:ListItem>
+    </asp:RadioButtonList>
+    <br />
+    <p><b>Space for luggage storage:</b></p>
+    <asp:RadioButtonList ID="spaceLuggage" EnableViewState="true" runat="server" align="center">
+     
+    <asp:ListItem Value="Excellent" runat="server" Text="Excellent"></asp:ListItem>
+    <asp:ListItem Value="Good" runat="server" Text="Good"></asp:ListItem>
+    <asp:ListItem Value="Fair" runat="server" Text="Fair"></asp:ListItem>
+    <asp:ListItem Value="Poor" runat="server" Text="Poor"></asp:ListItem>
+    <asp:ListItem Value="No Opinion" runat="server" Text="No Opinion"></asp:ListItem>
+    </asp:RadioButtonList>
+    <br />
+    <p><b>Comfort of seating</b></p>
+     <asp:RadioButtonList ID="Comfort" EnableViewState="true" runat="server" align="center">
+     
+    <asp:ListItem Value="Excellent" runat="server" Text="Excellent"></asp:ListItem>
+    <asp:ListItem Value="Good" runat="server" Text="Good"></asp:ListItem>
+    <asp:ListItem Value="Fair" runat="server" Text="Fair"></asp:ListItem>
+    <asp:ListItem Value="Poor" runat="server" Text="Poor"></asp:ListItem>
+    <asp:ListItem Value="No Opinion" runat="server" Text="No Opinion"></asp:ListItem>
+    </asp:RadioButtonList>
+    <br />
+    <p><b>Cleanliness of aircraft</b></p>
+   <asp:RadioButtonList ID="Cleanliness" EnableViewState="true" runat="server" align="center">
+     
+    <asp:ListItem Value="Excellent" runat="server" Text="Excellent"></asp:ListItem>
+    <asp:ListItem Value="Good" runat="server" Text="Good"></asp:ListItem>
+    <asp:ListItem Value="Fair" runat="server" Text="Fair"></asp:ListItem>
+    <asp:ListItem Value="Poor" runat="server" Text="Poor"></asp:ListItem>
+    <asp:ListItem Value="No Opinion" runat="server" Text="No Opinion"></asp:ListItem>
+    </asp:RadioButtonList>
+    
+    <br />
+    <p><b>Noise level of aircraft</b></p>
+    <asp:RadioButtonList ID="Noise" EnableViewState="true" runat="server" align="center">
+     
+    <asp:ListItem Value="Excellent" runat="server" Text="Excellent"></asp:ListItem>
+    <asp:ListItem Value="Good" runat="server" Text="Good"></asp:ListItem>
+    <asp:ListItem Value="Fair" runat="server" Text="Fair"></asp:ListItem>
+    <asp:ListItem Value="Poor" runat="server" Text="Poor"></asp:ListItem>
+    <asp:ListItem Value="No Opinion" runat="server" Text="No Opinion"></asp:ListItem>
+    </asp:RadioButtonList>
+    <br />
+    <br />
+    <br />
+    <asp:Button ID="sendSurvey" runat="server" Text="Send" />
+    
+    </form>
+    <asp:Literal ID="asms" runat="server" Visible="False" />
+
+    <br />
+    <br />
+    </div>
+  </div>
+</div>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        if (Page.IsPostBack)
+        {
+            Page.Validate();
+            if (Page.IsValid)
+            {
+
+                //transfers input fields to variables
+                form1.Visible = false;
+                asms.Visible = true;
+                string FlightNumber = flightNumber.Text;
+                string FlightDate = Date.Text;
+                string friendStaffs = friendStaff.SelectedValue;
+                string spaceLuggages = spaceLuggage.SelectedValue;
+                string comforts = Comfort.SelectedValue;
+                string Cleanlinesss = Cleanliness.SelectedValue;
+                string Noises = Noise.SelectedValue;
+
+               
+                //initiates db
+                SqlConnection dbConnection = new SqlConnection("Data Source=.\\SQLEXPRESS;Integrated Security=true");
+                try
+                {
+                    dbConnection.Open();
+                    dbConnection.ChangeDatabase("AirlineDatabase");
+
+                }
+                catch (SqlException exception)
+                {
+                    if (exception.Number == 911)
+                    {
+                        //if doesnt find create db
+                        SqlCommand sqlCommand = new SqlCommand("CREATE DATABASE AirlineDatabase", dbConnection);
+                        sqlCommand.ExecuteNonQuery();
+                        asms.Text = "<p><br />Successfully created the database.</p>";
+                        dbConnection.ChangeDatabase("AirlineDatabase");
+                    }
+                    else
+                        Response.Write("<p><br />Error code " + exception.Number
+                            + ": " + exception.Message + "</p>");
+                }
+                finally
+                {
+                    asms.Text += "<p><br />Successfully selected the database.</p>";
+                }
+                try
+                {
+                    string SQLString = "SELECT * FROM survey";
+                    SqlCommand checkIDTable = new SqlCommand(SQLString, dbConnection);
+                    SqlDataReader idRecords = checkIDTable.ExecuteReader();
+                    idRecords.Close();
+                }
+                catch (SqlException exception)
+                {
+                    if (exception.Number == 208)
+                    {
+                        //creates table
+                        SqlCommand sqlCommand = new SqlCommand("CREATE TABLE survey (FlightNumber VARCHAR(10), FlightDate VARCHAR(10), FriendStaff VARCHAR(10), SpaceLuggage VARCHAR(10), Comfort VARCHAR(10), Cleanliness VARCHAR(10), Noise VARCHAR(10)  )", dbConnection);
+                        sqlCommand.ExecuteNonQuery();
+                        asms.Text += "<p><br />Successfully created the table.</p>";
+                    }
+                    else
+                        asms.Text += "<p>Error code " + exception.Number
+                            + ": " + exception.Message + "</p>";
+                }
+                finally
+                {
+                    //inserts variables into db
+                    string surveyInfo = "INSERT INTO survey VALUES('"
+                        + FlightNumber + "', '"
+                        + FlightDate + "', '"
+                        + friendStaffs + "', '"
+                        + spaceLuggages + "', '" + comforts + "', '" + Cleanlinesss + "', '" + Noises + "')";
+                    SqlCommand sqlCommand = new SqlCommand(surveyInfo, dbConnection);
+                    sqlCommand.ExecuteNonQuery();
+                }
+
+                //creates text file with date
+                string log = @"C:\log" + DateTime.Now.ToString("dd-MM-yyyy hh-mm-ss") + ".txt";
+                using (FileStream fs = new FileStream(log, FileMode.Create))
+                {
+                    using (StreamWriter sw = new StreamWriter(fs))
+                    {
+                        //writes data into file
+                        sw.Write(FlightNumber + sw.NewLine);
+                        sw.Write(FlightDate + sw.NewLine);
+                        sw.Write(friendStaffs + sw.NewLine);
+                        sw.Write(spaceLuggages + sw.NewLine);
+                        sw.Write(comforts + sw.NewLine);
+                        sw.Write(Cleanlinesss + sw.NewLine);
+                        sw.Write(Noises + sw.NewLine);
+                    }
+                }
+
+
+                asms.Text += "<br /><p>Thank you for completing the survey!</p>";
+                dbConnection.Close();
+            }
+        }
+    }
+}
